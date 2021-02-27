0473e60d515faf49aa4ac0dcc28539f905945c27
Yogoda <joelfivat@hotmail.com>
2021-02-27 14:31:51 +0100

    cleanup

--- addons/visual_fsm/editor/vfsm_editor.gd
+++
@@ -1,9 +0,0 @@
-tool
-extends MarginContainer
-
-
-func edit(visual_fsm) -> void:
-	if not is_inside_tree():
-		yield(self, "tree_entered")
-	$VFSMGraphEdit.edit(visual_fsm.fsm)
-
--- addons/visual_fsm/vfsm_singleton.gd
+++
@@ -1,6 +0,0 @@
-tool
-class_name VFSMSingleton
-extends Node
-
-
-signal edit_custom_script(custom_script)
